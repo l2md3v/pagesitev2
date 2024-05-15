7f6a481efa52065ba88182ee3fe0bd9e8acc0051
l2md3v <lamdev1212@gmail.com>
2024-05-15 16:15:44 +0700

    banner

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   "discordId": "863408310903635998",
-  "banner": "",
+  "banner": "https://raw.githubusercontent.com/l2md3v/archive/master/weneedmakarov.gif",
   "theme_colors": ['#000000', '#ff0000'],
 };
