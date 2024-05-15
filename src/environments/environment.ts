a77b88af8487455240c0efa2bcd73a8569aa15b7
l2md3v <lamdev1212@gmail.com>
2024-05-15 15:45:54 +0700

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   "discordId": "863408310903635998",
+  "banner": "https://raw.githubusercontent.com/KhaiClone/archive/main/banner.gif",
   "theme_colors": ['#0501FA', '#FA0101'],
 };
