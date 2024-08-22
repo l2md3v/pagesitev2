f7e050d9350bcf3af20c8c9c8b4021ed10da664d
l2md3v <lamdev1212@gmail.com>
2024-08-22 14:23:31 +0700

    Change effect profile

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment = {
   "avatar_decoration": "a_3c97a2d37f433a7913a1c7b7a735d000",
   "theme_colors": ['#000000', '#ff0000'],
   "profile_effect": {
-    "intro": "https://cdn.discordapp.com/assets/profile_effects/effects/2024-07-30/vct_supernova/intro.png",
-    "loop": "https://cdn.discordapp.com/assets/profile_effects/effects/2024-07-26/vct_supernova/idle.png",
+    "intro": "https://cdn.discordapp.com/assets/profile_effects/effects/2024-08-08/of-ink-and-steel/intro.png",
+    "loop": "https://cdn.discordapp.com/assets/profile_effects/effects/2024-08-08/of-ink-and-steel/idle.png",
   }
 };
