98305d1b1ba1b798275a3a7a394f8de7a1f28f2f
l2md3v <lamdev1212@gmail.com>
2025-01-19 11:48:10 +0700

    change discordID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  "discordId": "863408310903635998",
+  "discordId": "895140562435440641",
   "banner": "https://raw.githubusercontent.com/l2md3v/archive/master/weneedmakarov.gif",
   "avatar_decoration": "a_3c97a2d37f433a7913a1c7b7a735d000",
   "theme_colors": ['#000000', '#ff0000'],
